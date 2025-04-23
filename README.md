8dabd1e9c8ce7d792e3f4f4a9301eb62ee2bbdbe
Alberto Sanchez <ing.albertosanchez@gmail.com>
2025-04-22 21:44:21 -0400

    git init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Chatbot-AI v1.0
+
+    Plugin de Wordpress para usar con la API de OpenAI y utilizarlo como un agente de inteligencia Artificial o chatbot que responda preguntas sobre un contexto definido.
+
+### Instalación
+
+Descarque el repositorio completo e instalelo en la sección de plugins de wordpress.
+
+### Configuración de la API de OpenAI
+
+...
---
+++ src/filters/add_link_settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Este filtro añade un enlace a la página de configuración del plugin en la lista de plugins instalados.
+ * Se utiliza el hook 'plugin_action_links_{plugin_basename}' para añadir el enlace.
+ */
+function chatbot_ai_add_link_settings($links) {
+    $ajustes_url = admin_url('settings.php?page=chatbot-ai-dashboard');
+    $nuevo_link = '<a href="' . esc_url($ajustes_url) . '">Settings</a>';
+    array_unshift($links, $nuevo_link); // lo ponemos al principio de la lista
+    return $links;
+}
