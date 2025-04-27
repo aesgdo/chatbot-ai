a1a339e077881218aee108bf9a3fe3a87dd45b40
Alberto Sanchez <ing.albertosanchez@gmail.com>
2025-04-27 09:50:14 -0400

    animation added

--- assets/js/chatbot_ai_script.js
+++ assets/js/chatbot_ai_script.js
@@ -1,20 +1,27 @@
 
 jQuery(window).on('load', () => {
     
+    /**
+     * * Chatbot AI Button Click Event
+     * * This function toggles the visibility of the chatbot AI wrapper and button.
+     */
     jQuery('.chatbot_ai_button').on('click', function() {
         
         jQuery('.chatbot_ai_wrapper').toggleClass('expanded');
         jQuery('.chatbot_ai_button').toggleClass('vanish opened');
-        jQuery('.chatbot_ai_container').toggleClass('expaned no-show');
+        jQuery('.chatbot_ai_container').toggleClass('expanded no-show');
 
         setTimeout(function() {
             jQuery('.chatbot_ai_button').toggleClass('vanish');
             jQuery('.chatbot_ai_button span').toggleClass('no-show');
-        }, 300);
-        
+        }, 300);       
+
     });
 
 
-
+     /* animation */
+     setInterval(function() {
+        jQuery('.chatbot_ai_button > span.message_icon').toggleClass('animated');
+    }, 5000);
 
 });
