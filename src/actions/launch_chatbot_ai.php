bcf5cfcefa971d9db1d3e88dbea8aeab2a16b5ae
Alberto Sanchez <ing.albertosanchez@gmail.com>
2025-04-27 10:32:09 -0400

    welcome msg added

--- src/actions/launch_chatbot_ai.php
+++ src/actions/launch_chatbot_ai.php
@@ -3,6 +3,9 @@
 
 function launch_chatbot_ai() {
 
+   $botface_image = plugin_dir_url(__FILE__) . '../../assets/images/botface300x300.png';
+   $botface_image = esc_url($botface_image);
+
    echo <<<EOD
    
       <div id="chatbot_ai_wrapper" class="chatbot_ai_wrapper botton_right">
@@ -10,7 +13,23 @@ function launch_chatbot_ai() {
          <div class="chatbot_ai_container no-show">
 
             <div class="chatbot_ai_header">
-               
+               <div class="chatbot_ai_image_wrap">
+                  <img style="width:100%" src="$botface_image"/>
+               </div>               
+               <span>Bienvenid@</span>
+            </div>
+
+            <div class="chatbot_ai_body">
+               <div class="chatbot_ai_body_message_wrap">
+                  <p>Hola, soy un asistente virtual. ¿Cuál es tu nombre?</p>
+               </div>
+               <div class="chatbot_ai_body_input_wrap">
+                  <input type="text" id="chatbot_ai_input" class="chatbot_ai_input" placeholder="Escribe tu nombre aquí...">
+                  <br>
+                  <button id="chatbot_ai_btn_start" class="chatbot_ai_btn_start">
+                   <span>Iniciar Chat</span>
+                  </button>
+               </div>
             </div>
                
          </div>
