463c2f5f8fd0169a85bbb47e5b14369e42ffafde
Alberto Sanchez <ing.albertosanchez@gmail.com>
2025-04-28 19:09:20 -0400

    ajax functions in progress

--- assets/js/chatbot_ai_script.js
+++ assets/js/chatbot_ai_script.js
@@ -1,4 +1,105 @@
 
+
+const sendMessageToChatbot = async (array_chat) => {
+
+    const url = './wp-content/plugins/chatbot-ai/src/api/index.php';
+
+    const data = {
+        chat   : array_chat,
+        target : 'openai_completions',
+    };
+    
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body : JSON.stringify(data),        
+    };
+
+    console.log(options);
+    console.log(array_chat);
+    
+    const server = await fetch(url, options);
+    const response = await server.json();
+
+}
+
+/**
+ * Esta funcion agrega el mensaje del usuario a la cola de mensajes
+ * esta cola es un array que se va llenando con los mensajes del usuario y el bot
+ * @param {string} user_message 
+ */
+const enqueueUserMessages = (user_message) => {
+       
+    let array_chat = [];
+
+    if (
+        typeof localStorage.chat === 'undefined' || 
+               localStorage.chat === null        || 
+               localStorage.chat === ""
+       ){
+
+        array_chat = [
+            {
+                "role"      : "assistant",
+                "content"   : "Hola, soy un asistente virtual. ¿Cuál es tu nombre?"
+            },
+            {
+                "role"      : "user",
+                "content"   : user_message
+            },
+        ];
+
+        localStorage.chat = JSON.stringify(array_chat);
+
+    }
+    else{
+
+        array_chat = JSON.parse(localStorage.chat);
+
+        const new_msg_obj = {"role":"user","content":user_message};
+
+        array_chat[array_chat.length] = new_msg_obj;
+
+        localStorage.chat = JSON.stringify(array_chat);
+        
+    }
+
+    sendMessageToChatbot(array_chat);
+
+}
+
+const handleUserInput = () => {
+    
+    const chatbot_ai_input = jQuery('.chatbot_ai_input');
+    const user_message = chatbot_ai_input.val(); chatbot_ai_input.val("");
+    //console.log(user_message);
+    
+    const lastchild = jQuery('.chatbot_ai_body_message_wrap').children().last();
+    
+    if (user_message.length > 0) {
+
+        setTimeout(function() {
+
+            jQuery('.chatbot_ai_body_message_wrap').append(`
+
+                <div class="user">
+                    <p>
+                        ${ ! lastchild.hasClass('user') ? '<span class="title">Yo:</span><br>' : ''}
+                        <span class="msg">${user_message}</span>
+                    </p>
+                </div>`
+            );
+
+            enqueueUserMessages(user_message);
+
+        },300);
+
+    }
+
+}
+
 jQuery(window).on('load', () => {
     
     /**
@@ -11,10 +112,12 @@ jQuery(window).on('load', () => {
         jQuery('.chatbot_ai_button').toggleClass('vanish opened');
         jQuery('.chatbot_ai_container').toggleClass('expanded no-show');
 
-        setTimeout(function() {
+        setTimeout(function() {            
             jQuery('.chatbot_ai_button').toggleClass('vanish');
             jQuery('.chatbot_ai_button span').toggleClass('no-show');
         }, 300);       
+
+        jQuery('.chatbot_ai_input').removeClass('user_name_error');
 
     });
 
@@ -24,44 +127,80 @@ jQuery(window).on('load', () => {
         jQuery('.chatbot_ai_button > span.message_icon').toggleClass('animated');
     }, 5000);
 
-
-    jQuery('.chatbot_ai_btn_start').on('click', function() {
+    /**
+     * Este evento inicia el chat
+     */
+    jQuery('.chatbot_ai_btn_start').on('click', function(ev) {
         console.log('start button clicked');
 
-        jQuery('.chatbot_ai_header').toggleClass('vanish');
+        let user_name = jQuery('.chatbot_ai_input').val();
 
-        jQuery('.chatbot_ai_btn_start span').toggleClass('vanish no-show');
+        // si el nombre de usuario es menor a 1, entonces se muestra un error
+        if ( user_name.length < 1){
+            jQuery('.chatbot_ai_input').addClass('user_name_error');
+            jQuery('.chatbot_ai_input').focus();
+        }
+        else{
 
-        jQuery('.chatbot_ai_body_input_wrap').toggleClass('vanish');
+            jQuery('.chatbot_ai_input').removeClass('user_name_error');
 
-        jQuery('.chatbot_ai_body').toggleClass('vanish');
+            // si el chart no ha comenzado, entonces se inicia el chat
+            if ( ! jQuery('.chatbot_ai_btn_start').hasClass('chat_started')) {
+                
+                // agregamos un evento para que al presionar enter se envíe el mensaje
+                jQuery(window).on('keydown', function(ev) {
+                    // console.log(ev.keyCode);
+                    if (ev.keyCode === 13) {
+                        ev.preventDefault();
+                        handleUserInput();
+                    }
+                });
+
+                jQuery('.chatbot_ai_header').toggleClass('vanish');
+                jQuery('.chatbot_ai_btn_start span').toggleClass('vanish no-show');
+                jQuery('.chatbot_ai_body_input_wrap').toggleClass('vanish');
         
-        setTimeout(function() {
+                jQuery('.chatbot_ai_body').toggleClass('vanish');
+                
+                setTimeout(function() {
+                    
+                    jQuery('.welcome_message').toggleClass('no-show');
+                    jQuery('.chatbot').toggleClass('no-show');
+                    //jQuery('.user').toggleClass('no-show');
+                    
+                    jQuery('.chatbot_ai_btn_start').addClass('chat_started');
+                    jQuery('.chatbot_ai_btn_start span').toggleClass('vanish');            
+                    
+        
+                    jQuery('.chatbot_ai_body').toggleClass('vanish chat_started');
+        
+                    // TODO cambiar toggleclass por addclass
+                    jQuery('.chatbot_ai_header').toggleClass('vanish no-show');
+        
+                    jQuery('.chatbot_ai_body_input_wrap').toggleClass('vanish');
+                    
+                    jQuery('.chatbot_ai_header').toggleClass('chat_started');
+                    jQuery('.chatbot_ai_input').toggleClass('chat_started');
+                    jQuery('.chatbot_ai_body_input_wrap').toggleClass("chat_started");
+                    
+                    jQuery('.chatbot_ai_input').attr('placeholder', 'Escribe tu mensaje...');
+        
+        
+                    // TODO cambiar toggleclass por addclass
+                }, 300);
+            }
+            else{
+                ev.preventDefault();
+            }
 
-            
-            jQuery('.welcome_message').toggleClass('no-show');
-            jQuery('.chatbot').toggleClass('no-show');
-            jQuery('.user').toggleClass('no-show');
+        }
+        
+        handleUserInput();
+        
 
-            jQuery('.chatbot_ai_body').toggleClass('vanish chat_started');
-
-            // TODO cambiar toggleclass por addclass
-            jQuery('.chatbot_ai_header').toggleClass('vanish no-show');
-
-            jQuery('.chatbot_ai_btn_start span').toggleClass('vanish');
-            jQuery('.chatbot_ai_body_input_wrap').toggleClass('vanish');
-            
-            jQuery('.chatbot_ai_header').toggleClass('chat_started');
-            jQuery('.chatbot_ai_btn_start').toggleClass('chat_started');
-            jQuery('.chatbot_ai_input').toggleClass('chat_started');
-            jQuery('.chatbot_ai_body_input_wrap').toggleClass("chat_started");
-            
-            jQuery('.chatbot_ai_input').attr('placeholder', 'Escribe tu mensaje...');
-
-
-            // TODO cambiar toggleclass por addclass
-        }, 300);
     });
 
+    
 
 });
+
